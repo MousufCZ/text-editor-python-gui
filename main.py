4809388a35673edf7b85b7c94a62cd717d082b42
Mousuf C Zaman <mousuf@live.co.uk>
2024-09-14 00:32:40 +0100

    Code formatting

--- main.py
+++ main.py
@@ -14,14 +14,17 @@ heading.pack()
 scrollbar = Scrollbar(stimulator_window)
 scrollbar.pack(side=RIGHT, fill=Y)
 
-Editor = Text(stimulator_window, width=200, height=250, yscrollcommand=scrollbar.set)
+Editor = Text(stimulator_window, width=200, height=250, 
+              yscrollcommand=scrollbar.set)
 Editor.pack(fill=BOTH)
 
 scrollbar.config(command=Editor.yview)
 
 # Save() function
 def save():
-    filepath = asksaveasfilename(defaultextension="txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    filepath = asksaveasfilename(defaultextension="txt", 
+                                 filetypes=[("Text Files", "*.txt"), 
+                                            ("All Files", "*.*")])
     if not filepath:
         return
     with open(filepath, "w") as output_file:
@@ -30,7 +33,8 @@ def save():
     stimulator_window.title(f"Entitled - {filepath}")
 
 # Creating Buttons
-button = Button(stimulator_window, text='Save', font=('normal', 10), command=save, bg='yellow')
+button = Button(stimulator_window, text='Save', font=('normal', 10),
+                command=save, bg='yellow')
 button.pack()  # Use pack for layout management
 
 # Command to run the main loop
