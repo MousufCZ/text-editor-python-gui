b83597e47f3552093058506564395b1d6ea429cd
Mousuf C Zaman <mousuf@live.co.uk>
2024-10-14 22:15:44 +0100

    Network.

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ button.pack()  # Use pack for layout management
 stimulator_window.mainloop()
 
 '''
-I need to add some other features on to this. Google solutions
-for what to add.
+I need to add some other features on to this. 
+Google solutions for what to add.
 
 Think about
 '''
